89123c01c757468c07993754c3549430c669118b
Nikita Bechthold <75571915+TerraNova4697@users.noreply.github.com>
2023-12-08 09:30:52 +0600

    Initial commit

---
+++ app/conf/stage.py
@@ -0,0 +1,39 @@
+import os
+
+from .common import * # noqa
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DEBUG = os.environ.get('DEBUG')
+PRODUCTION = os.environ.get('PRODUCTION')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', # noqa
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), # noqa
+    os.path.join(BASE_DIR, 'media'), # noqa
+]
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # noqa
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles') # noqa
---
+++ app/context_processor.py
@@ -0,0 +1,7 @@
+
+
+def project_context(request):
+    context = {
+        'app_email': 'nikitabechthold@gmail.com'
+    }
+    return context
